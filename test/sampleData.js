979b73ce5922481e936f361058d89ca2d1c1ef08
Parashuram <me@nparashuram.com>
2012-11-03 16:52:57 -0700

    Made the tests to run consistantly
    - Removed extra start statements
    - Unique Int values (for keys and indexes) generated

--- test/sampleData.js
+++ test/sampleData.js
@@ -1,23 +1,33 @@
 var DB = {
-	NAME: "dbName",
-	OBJECT_STORE_1: "objectStore1",
-	OBJECT_STORE_2: "objectStore2",
-	OBJECT_STORE_3: "objectStore3",
-	OBJECT_STORE_4: "objectStore4",
-	INDEX1_ON_OBJECT_STORE_1: "Index1_ObjectStore1",
-	INDEX1_ON_OBJECT_STORE_2: "Index1_ObjectStore2"
+	NAME : "dbName",
+	OBJECT_STORE_1 : "objectStore1",
+	OBJECT_STORE_2 : "objectStore2",
+	OBJECT_STORE_3 : "objectStore3",
+	OBJECT_STORE_4 : "objectStore4",
+	INDEX1_ON_OBJECT_STORE_1 : "Index1_ObjectStore1",
+	INDEX1_ON_OBJECT_STORE_2 : "Index1_ObjectStore2"
 };
 
-var sample = {
-	obj: function(){
-		return {
-			"String": "Sample " + new Date(),
-			"Int": this.integer(),
-			"Float": Math.random(),
-			"Boolean": true
+var sample = (function() {
+
+	var takenValues = {};
+	return {
+		obj : function() {
+			return {
+				"String" : "Sample " + new Date(),
+				"Int" : this.integer(),
+				"Float" : Math.random(),
+				"Boolean" : true
+			}
+		},
+		integer : function(arg) {
+			var res;
+			do {
+				console.log("Inside the loop")
+				res = parseInt(Math.random() * (arg || 1000));
+			} while (takenValues[res]);
+			takenValues[res] = true;
+			return res;
 		}
-	},
-	integer: function(arg){
-		return parseInt(Math.random() * (arg || 100));
-	}
-};
+	};
+}());
